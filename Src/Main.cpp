6984e1e9edcd16a6916f9d87e17cadbf82501dd6
bobnone <shurikencraft@gmail.com>
2022-05-14 09:00:53 -0500

    Initial commit part 2

---
+++ Src/Main.cpp
@@ -0,0 +1,20 @@
+// ----------------------------------------------------------------
+// From Game Programming in C++ by Sanjay Madhav
+// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
+// 
+// Released under the BSD License
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+
+#include "Game.h"
+
+int main(int argc, char** argv)
+{
+	Game game;
+	if(game.Initialize())
+	{
+		game.RunLoop();
+	}
+	game.Shutdown();
+	return 0;
+}
